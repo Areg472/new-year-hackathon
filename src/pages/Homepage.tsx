ad507d7ade45ab7abfd2af9ff5164c9153d100b8
Aregus <74340447+Areg472@users.noreply.github.com>
2024-12-13 10:32:24 +0400

    initial

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import MetaTags from "../Components/MetaTags.tsx";
+
+export function Homepage() {
+    const [count, setCount] = useState(0)
+    return (
+        <>
+            <MetaTags
+                title="Areg travels"
+                description="My website for codedex hackathon"
+                image="https://utfs.io/f/QI8sw9xDXLhGN8UiSz2DsRLvQdntFY8ehkO5W3Sr0cPpU2Kq"
+                name="Areg travels"
+            />
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
+}

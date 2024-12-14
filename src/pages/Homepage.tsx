be6d6c166b002c4b464ba9bf8d63d77b37865f67
Aregus <74340447+Areg472@users.noreply.github.com>
2024-12-14 15:12:53 +0400

    hmm

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -17,7 +17,7 @@ export function Homepage() {
                 <img className="w-20 h-20 ml-[50%] translate-x-[-50%] mb-4 border-black border-double border-4" src="https://utfs.io/f/QI8sw9xDXLhGwi2l69SRX6DVsvJiC2qWgOU98GMQjlmH1BrL" alt="globe"/>
                 <h1>Areg travels</h1>
                 <p className="text-xs mt-4">Welcome to Areg travels! Here you can explore flights and tours with Areg!</p>
-                <div className="flex flex-col md:flex-row md:mb-16">
+                <div className="flex flex-col md:flex-row mb-16 md:mb-0">
                     <div className="basis-1/3 mt-16">
                         <p>Tours starting at 10$!!!</p>
                         <Link to="/Tours">
